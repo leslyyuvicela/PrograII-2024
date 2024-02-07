8081a0d4fbd0ba48fc61e0da92bc1b2de9d8f4f4
leslyyuvicela <yuvicelaguzman14727@gmail.com>
2024-02-07 18:47:50 +0100

    conversor de longitud

--- ControlesBasicos/app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ ControlesBasicos/app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -7,69 +7,56 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabHost tbh;
     TextView tempVal;
-    RadioGroup opt;
     Button btn;
     Spinner spn;
+    conversores objConversor = new conversores();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("LONGITUD").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("ALMACENAMIENTO").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+        tbh.addTab(tbh.newTabSpec("MONEDAS").setContent(R.id.tabMonedas).setIndicator("MONEDAS",null));
+
+        btn = findViewById(R.id.btnLongitudConvertir);
         btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try{
-                    opt = findViewById(R.id.optCalculadora);
-                    spn = findViewById(R.id.spnOpciones);
+           @Override
+           public void onClick(View view) {
+              spn = findViewById(R.id.spnLongitudDe);
+              int  de = spn.getSelectedItemPosition();
 
-                    tempVal = findViewById(R.id.txtnum1);
-                    double num1 = Double.parseDouble(tempVal.getText().toString());
+              spn = findViewById(R.id.spnLongitudA);
+              int a = spn.getSelectedItemPosition();
 
-                    tempVal = findViewById(R.id.txtnum2);
-                    double num2 = Double.parseDouble(tempVal.getText().toString());
+              tempVal = findViewById(R.id.txtLongitudCantidad);
+              double cantidad = Double.parseDouble(tempVal.getText().toString());
 
-                    double resp = 0;
-                        /*switch (opt.getCheckedRadioButtonId()){
-                            case R.id.optSuma:
-                                resp = num1+num2;
-                                break;
-                            case R.id.optResta:
-                                resp = num1-num2;
-                                break;
-                            case R.id.optMultiplicacion:
-                                resp=num1*num2;
-                                break;
-                            case R.id.optDivision:
-                                resp = num1/num2;
-                                break;
-                        }*/
-                    switch (spn.getSelectedItemPosition()){
-                        case 0:
-                            resp = num1+num2;
-                            break;
-                        case 1:
-                            resp = num1-num2;
-                            break;
-                        case 2:
-                            resp = num1*num2;
-                            break;
-                        case 3:
-                            resp = num1/num2;
-                            break;
-                    }
-                    tempVal = findViewById(R.id.lblrespuesta);
-                    tempVal.setText("Suma: " + resp );
-                }catch (Exception e){
-                    tempVal = findViewById(R.id.lblrespuesta);
-                    tempVal.setText("Error: "+ e.getMessage());
-                }
-            }
-        });
+              double resp = objConversor.convertir(0, de, a, cantidad);
+               Toast.makeText(getApplicationContext(),"Respuesta:" +
+                       resp, Toast.LENGTH_LONG).show();
 
+           }
+       });
+    }
+}
+class conversores{
+    double[][] valores = {
+            {1, 100, 39.3701, 3.28084166667,1.193, 1.0936138888889999077, 0.001, 0.000621371}
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
