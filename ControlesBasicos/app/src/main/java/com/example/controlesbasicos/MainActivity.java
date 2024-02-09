0cfa32ed90097db19212585871d6a70688d94ca3
leslyyuvicela <yuvicelaguzman14727@gmail.com>
2024-02-09 15:41:30 -0600

    Conversor de almacenamiento y longitud terminado

--- ControlesBasicos/app/src/main/java/com/example/controlesbasicos/MainActivity.java
+++ ControlesBasicos/app/src/main/java/com/example/controlesbasicos/MainActivity.java
@@ -50,6 +50,25 @@ public class MainActivity extends AppCompatActivity {
 
            }
        });
+        btn = findViewById(R.id.btnAlmacenamientoConvertir);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spn = findViewById(R.id.spnAlmacenamientoDe);
+                int  de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnAlmacenamientoA);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtAlmacenamientoCantidad);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+                double resp =1/ objConversor.convertir(1, de, a, cantidad);
+                Toast.makeText(getApplicationContext(),"Respuesta:" +
+                        resp, Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
 class conversores{
@@ -58,8 +77,8 @@ class conversores{
             {1, 100, 39.3701, 3.28084166667,1.193, 1.0936138888889999077, 0.001, 0.000621371, 0.001,0.000001, 0.000000001},
 
             //ALMACENAMIENTO
-            {1, 8,1000*8, Math.pow(1000,2)*8, Math.pow(1000,3)*8, Math.pow(1000,4)*8, Math.pow(1000,5)*8, Math.pow(1000,6)*8, Math.pow(1000,7)*8
-                    1000*8, Math.pow(1000,2)*8, Math.pow(1000,3)*8, Math.pow(1000,4)*8, Math.pow(1000,5)*8, Math.pow(1000,6)*8, Math.pow(1000,7)*8 }
+            {1, 8,1000*8, Math.pow(1000,2)*8, Math.pow(1000,3)*8, Math.pow(1000,4)*8, Math.pow(1000,5)*8, Math.pow(1000,6)*8, Math.pow(1000,7)*8,
+                    1024*8, Math.pow(1024,2)*8, Math.pow(1024,3)*8, Math.pow(1024,4)*8, Math.pow(1024,5)*8, Math.pow(1024,6)*8, Math.pow(1024,7)*8 }
 
     };
     public double convertir(int opcion, int de, int a, double cantidad){
